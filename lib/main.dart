3666a5d54e44f479dc47d4a02d5e4c3531859bb2
Varun-Kolanu <kolanuvarun739@gmail.com>
2024-03-25 23:16:23 +0530

    Added object detection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:tflite_flutter/tflite_flutter.dart' as tfl;
+import 'package:tflite_v2/tflite_v2.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -25,55 +27,145 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  double result = 0.0;
+  File _image = File('');
+  List? _recognitions;
+  late double _imageHeight;
+  late double _imageWidth;
+  bool _busy = false;
 
-  runModel(double no) async {
-    final interpreter =
-        await tfl.Interpreter.fromAsset('assets/number_predictor.tflite');
-    final input = [
-      [no]
-    ];
-    var output = List.filled(1, 0).reshape([1, 1]);
-    interpreter.run(input, output);
-    result = output[0][0];
-    print(output);
+  Future pickImageAndPredict() async {
+    var image = await ImagePicker().pickImage(source: ImageSource.camera);
     setState(() {
-      result = output[0][0];
+      _busy = true;
     });
-    // return  as double;
+    predictImage(File(image!.path));
   }
 
-  TextEditingController noController = TextEditingController();
+  Future predictImage(File image) async {
+    FileImage(image)
+        .resolve(const ImageConfiguration())
+        .addListener(ImageStreamListener((ImageInfo info, bool _) {
+      setState(() {
+        _imageHeight = info.image.height.toDouble();
+        _imageWidth = info.image.width.toDouble();
+      });
+    }));
+
+    var recognitions = await Tflite.detectObjectOnImage(
+      path: image.path,
+      numResultsPerClass: 1,
+    );
+
+    setState(() {
+      _image = File(image.path);
+      _recognitions = recognitions;
+      _busy = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _busy = true;
+
+    loadModel().then((val) {
+      setState(() {
+        _busy = false;
+      });
+    });
+  }
+
+  Future loadModel() async {
+    Tflite.close();
+    try {
+      String? res = await Tflite.loadModel(
+        model: "assets/model.tflite",
+        labels: "assets/labels.txt",
+        // useGpuDelegate: true,
+      );
+      return res;
+    } on PlatformException {
+      print('Failed to load model.');
+    }
+  }
+
+  List<Widget> renderBoxes(Size screen) {
+    if (_recognitions == null) return [];
+
+    double factorX = screen.width;
+    double factorY = _imageHeight / _imageWidth * screen.width;
+    Color blue = const Color.fromRGBO(37, 213, 253, 1.0);
+    return _recognitions!.map((re) {
+      if (re["confidenceInClass"] < 0.5) {
+        return Container();
+      }
+      return Positioned(
+        left: re["rect"]["x"] * factorX,
+        top: re["rect"]["y"] * factorY,
+        width: re["rect"]["w"] * factorX,
+        height: re["rect"]["h"] * factorY,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+            border: Border.all(
+              color: blue,
+              width: 2,
+            ),
+          ),
+          child: Text(
+            "${re["detectedClass"]} ${(re["confidenceInClass"] * 100).toStringAsFixed(0)}%",
+            style: TextStyle(
+              background: Paint()..color = blue,
+              color: Colors.white,
+              fontSize: 12.0,
+            ),
+          ),
+        ),
+      );
+    }).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    List<Widget> stackChildren = [];
+
+    stackChildren.add(Positioned(
+      top: 0.0,
+      left: 0.0,
+      width: size.width,
+      child: _image.path.isEmpty
+          ? const Text('No image selected.')
+          : Image.file(_image),
+    ));
+
+    stackChildren.addAll(renderBoxes(size));
+
+    if (_busy) {
+      stackChildren.add(const Opacity(
+        opacity: 0.3,
+        child: ModalBarrier(
+          dismissible: false,
+          color: Colors.grey,
+        ),
+      ));
+      stackChildren.add(const Center(child: CircularProgressIndicator()));
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           "Tflite sample",
         ),
       ),
-      body: Column(
-        children: [
-          TextFormField(
-            controller: noController,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: MaterialButton(
-              child: const Text(
-                "Run",
-              ),
-              onPressed: () {
-                runModel(double.parse(noController.text));
-              },
-            ),
-          ),
-          Text(
-            result.toString(),
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          )
-        ],
+      body: Stack(
+        children: stackChildren,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: pickImageAndPredict,
+        tooltip: 'Pick Image',
+        child: const Icon(Icons.image),
       ),
     );
   }
